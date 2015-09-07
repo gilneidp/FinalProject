85816940d619d359570cbec60ae5ec7e336f7458
gilneidp <gilnei@gilnei.com.br>
2015-09-07 11:38:09 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ $(document).ready(function() {
     $("a[href*='" + page + "']").parent().addClass("active");
   });
 });
+$(document).ready(function() {
+  $.get("leftmenu.html", function(data) { 
+    $("#leftmenu-menu").append(data); 
+    var page = window.location.href.slice(window.location.href.lastIndexOf("/")+1);
+    $("a[href*='" + page + "']").parent().addClass("active");
+  });
+});
