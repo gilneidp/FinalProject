a8d6acc3e508c0305679ad4a1aee78bd4aab8cbd
gilneidp <gilnei@gilnei.com.br>
2015-09-09 09:19:11 -0300

    Update views.py

--- madapp/mad/views.py
+++ madapp/mad/views.py
@@ -1,6 +1,17 @@
+import os
+import datetime
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from django.template import RequestContext
- 
+from array import *
+from .models import *
+from django.db.models import F
+from datetime import timedelta
+from django.utils import timezone
+from django.core import serializers
+
 def index(request):
     return render_to_response('index.html', context_instance=RequestContext(request))
